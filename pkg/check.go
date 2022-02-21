afae4b646775be1335a030692ac769d0be577133
ali <a_icam@yahoo.com>
2022-02-21 22:25:30 +0330

    fix: non-persian alphabet handled, bug fixed

--- pkg/check.go
+++ pkg/check.go
@@ -3,6 +3,7 @@ package pkg
 func Correct(sentence string) string {
 	return reverse(string(CorrectRune([]rune(sentence), 0, nil)))
 }
+
 func reverse(s string) string {
 	runes := []rune(s)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
@@ -10,9 +11,70 @@ func reverse(s string) string {
 	}
 	return string(runes)
 }
+
 func CorrectRune(sentence []rune, pointer int, correctStr []rune) []rune {
+
+	// context define a 3 letter combination of each rune
+	context := FullContext{}
+
+	// first of all check if there is a non-persian character in the context
+	if pointer < len(sentence)-1 {
+		// check if the rune is Persian
+		if _, ok := CharacterToUnicodeMap[sentence[pointer]]; !ok {
+			correctStr = append(correctStr, sentence[pointer])
+			return CorrectRune(sentence, pointer+1, correctStr)
+		}
+
+		// check if the next rune is not persian
+		if _, ok := CharacterToUnicodeMap[sentence[pointer+1]]; !ok {
+			context.IsPostRunePersian = false
+			context.IsPostRuneConnectable = false
+		} else {
+			context.IsPostRunePersian = true
+			if CharacterConnectable[sentence[pointer+1]][IsConnectableBefore] {
+				context.IsPostRuneConnectable = true
+			} else {
+				context.IsPostRuneConnectable = false
+			}
+		}
+	} else {
+		context.IsPostRunePersian = false
+		context.IsPostRuneConnectable = false
+	}
+
+	// check if the previous rune is not persian
+	if pointer != 0 {
+		if _, ok := CharacterToUnicodeMap[sentence[pointer-1]]; !ok {
+			context.IsPreRunePersian = false
+			context.IsPreRuneConnectable = false
+		} else {
+			context.IsPreRunePersian = true
+			if CharacterConnectable[sentence[pointer-1]][IsConnectableAfter] {
+				context.IsPreRuneConnectable = true
+			} else {
+				context.IsPreRuneConnectable = false
+			}
+		}
+	} else {
+		context.IsPreRunePersian = false
+		context.IsPreRuneConnectable = false
+	}
+
+	if pointer == 0 {
+		if context.IsPostRunePersian && sentence[pointer+1] != ' ' {
+			if CharacterConnectable[sentence[pointer]][IsConnectableAfter] {
+				correctStr = append(correctStr, CharacterToUnicodeMap[sentence[pointer]][SpaceFirstCharacterLast])
+			} else {
+				correctStr = append(correctStr, CharacterToUnicodeMap[sentence[pointer]][SpaceFirstSpaceLast])
+			}
+		} else {
+			correctStr = append(correctStr, CharacterToUnicodeMap[sentence[pointer]][SpaceFirstSpaceLast])
+		}
+		return CorrectRune(sentence, pointer+1, correctStr)
+	}
+
 	if pointer == len(sentence)-1 {
-		if CharacterConnectable[sentence[pointer-1]][IsConnectableAfter] {
+		if context.IsPreRunePersian && context.IsPreRuneConnectable {
 			correctStr = append(correctStr, CharacterToUnicodeMap[sentence[pointer]][CharacterFirstSpaceLast])
 		} else {
 			correctStr = append(correctStr, sentence[pointer])
@@ -20,42 +82,24 @@ func CorrectRune(sentence []rune, pointer int, correctStr []rune) []rune {
 		return correctStr
 	}
 
-	if pointer == 0 && sentence[pointer+1] != ' ' {
-		if CharacterConnectable[sentence[pointer]][IsConnectableAfter] {
-			correctStr = append(correctStr, CharacterToUnicodeMap[sentence[pointer]][SpaceFirstCharacterLast])
-		}
-		return CorrectRune(sentence, pointer+1, correctStr)
-	}
-
-	// handle starting space
-	if sentence[pointer-1] == ' ' {
-		correctStr = append(correctStr, CharacterToUnicodeMap[sentence[pointer]][SpaceFirstCharacterLast])
-		return CorrectRune(sentence, pointer+1, correctStr)
-	}
-
 	// Check if before and after the current alphabet are connectable alphabets
-	if CharacterConnectable[sentence[pointer-1]][IsConnectableAfter] &&
-		CharacterConnectable[sentence[pointer+1]][IsConnectableBefore] {
+	if context.IsPreRuneConnectable && context.IsPostRuneConnectable {
 		correctStr = append(correctStr, CharacterToUnicodeMap[sentence[pointer]][CharacterFirstCharacterLast])
 	}
 
 	// Check if before is connectable and after not
-	if CharacterConnectable[sentence[pointer-1]][IsConnectableAfter] &&
-		!CharacterConnectable[sentence[pointer+1]][IsConnectableBefore] {
+	if context.IsPreRuneConnectable && !context.IsPostRuneConnectable {
 		correctStr = append(correctStr, CharacterToUnicodeMap[sentence[pointer]][CharacterFirstSpaceLast])
 	}
 
 	// Check if before is not connectable and after is
-	if !CharacterConnectable[sentence[pointer-1]][IsConnectableAfter] &&
-		CharacterConnectable[sentence[pointer+1]][IsConnectableBefore] {
+	if !context.IsPreRuneConnectable && context.IsPostRuneConnectable {
 		correctStr = append(correctStr, CharacterToUnicodeMap[sentence[pointer]][SpaceFirstCharacterLast])
 	}
 
 	// Check if before and after are not connectable
-	if sentence[pointer-1] != ' ' &&
-		!CharacterConnectable[sentence[pointer-1]][IsConnectableAfter] &&
-		!CharacterConnectable[sentence[pointer+1]][IsConnectableBefore] {
-		correctStr = append(correctStr, CharacterToUnicodeMap[sentence[pointer]][SpaceFirstSpaceLast])
+	if !context.IsPreRuneConnectable && !context.IsPostRuneConnectable {
+		correctStr = append(correctStr, sentence[pointer])
 	}
 
 	return CorrectRune(sentence, pointer+1, correctStr)
---
+++ pkg/structures.go
@@ -0,0 +1,8 @@
+package pkg
+
+type FullContext struct {
+	IsPreRunePersian      bool
+	IsPostRunePersian     bool
+	IsPreRuneConnectable  bool
+	IsPostRuneConnectable bool
+}
